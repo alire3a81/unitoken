c0282b0b39e87a5cf5c2c95f9ee716245d5b2666
Alireza <90978029+alire3a81@users.noreply.github.com>
2023-07-19 17:30:49 +0300

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x884daf886f95078f3243f4cbb5123f7d915dbdf8";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
